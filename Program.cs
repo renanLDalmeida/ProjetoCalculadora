04c2895867eba794b7860fa5f1baf88837abba50
RenanLDalmeida <rdalmeidi@gmail.com>
2020-07-16 14:15:56 -0300

    Projeto iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
